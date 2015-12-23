fb2fde88f638195598cab6a389397732955e10d3
shiro16 <nyanyanyawan24@gmail.com>
2015-12-23 23:40:32 +0900

    bundle gem

---
+++ lib/log/analyzer.rb
@@ -0,0 +1,7 @@
+require "log/analyzer/version"
+
+module Log
+  module Analyzer
+    # Your code goes here...
+  end
+end
---
+++ lib/log/analyzer/version.rb
@@ -0,0 +1,5 @@
+module Log
+  module Analyzer
+    VERSION = "0.1.0"
+  end
+end
