8e6e663288270f42d305bee69cfc16e6db4a81d0
shiro16 <nyanyanyawan24@gmail.com>
2015-12-29 00:57:24 +0900

    add log class

---
+++ lib/log-analyzer/log.rb
@@ -0,0 +1,18 @@
+module Log::Analyze
+  class Log
+    def initialize(path)
+      @file = File.open(path)
+    end
+
+    def each_line(&block)
+      @file.each_line do |line|
+        md = line.match(/Started (?<request_method>GET|POST|DELETE|PATCH|PUT) "(?<path_info>[^"]+)"/)
+        env = md.names.inject({}) do |hash, name|
+                hash[name.upcase] = md[name]
+                hash
+              end
+        block.call(ActionDispatch::Request.new(env))
+      end
+    end
+  end
+end
