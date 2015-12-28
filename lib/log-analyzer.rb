28a8f589ac8da90c6f558a075f96c1d9430c8396
shiro16 <nyanyanyawan24@gmail.com>
2015-12-29 00:58:13 +0900

    require any classes

--- lib/log-analyzer.rb
+++ lib/log-analyzer.rb
@@ -1,7 +1,11 @@
-require "log-analyzer/version"
+require 'log-analyzer/version'
+require 'log-analyzer/routes'
+require 'log-analyzer/analyze'
+require 'log-analyzer/log'
+require 'log-analyzer/mapper'
+require 'log-analyzer/pattern'
 
 module Log
   module Analyzer
-    # Your code goes here...
   end
 end
