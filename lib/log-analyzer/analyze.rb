26c4063534d2800163974184ac8f8c4dad8ea2d1
shiro16 <nyanyanyawan24@gmail.com>
2015-12-30 00:47:53 +0900

    fix analyze class

--- lib/log-analyzer/analyze.rb
+++ lib/log-analyzer/analyze.rb
@@ -1,14 +1,42 @@
 module Log::Analyzer
   class Analyze
     def initialize(files, routes_text)
-      routes = Log::Analyzer::Routes.new(routes_text)
-      mapper = Log::Analyzer::Mapper.new(routes)
-      files.each do |file|
+      files = [files] unless files.is_a?(Array)
+      @files = files
+      @routes = Log::Analyzer::Routes.new(routes_text)
+    end
+
+    def result
+      @result ||= matche
+    end
+
+    def sort
+      result.sort_by{ |key, val| -val }.to_h
+    end
+
+    private
+    def router
+      @router ||= Log::Analyzer::Router.new(@routes)
+    end
+
+    def matche
+      @files.inject(format) do |result, file|
         log = Log::Analyze::Log.new(file)
         log.each_line do |req|
-          next unless matche = mapper.serve(req)
-          
+          matche = router.serve(req)
+          next unless matche.any?
+
+          _, _, route =  matche.first
+          result[route.path.uri_pattern] += 1
         end
+        result
+      end
+    end
+
+    def format
+      @routes.inject(Hash.new(0)) do |hash, route|
+        hash[route.path.uri_pattern] = 0
+        hash
       end
     end
   end
