072185d3524cb5c02e136b0efc887a48d7763e38
shiro16 <nyanyanyawan24@gmail.com>
2015-12-30 00:44:58 +0900

    rename mapper

--- lib/log-analyzer.rb
+++ lib/log-analyzer.rb
@@ -2,7 +2,7 @@ require 'log-analyzer/version'
 require 'log-analyzer/routes'
 require 'log-analyzer/analyze'
 require 'log-analyzer/log'
-require 'log-analyzer/mapper'
+require 'log-analyzer/router'
 require 'log-analyzer/pattern'
 
 module Log
