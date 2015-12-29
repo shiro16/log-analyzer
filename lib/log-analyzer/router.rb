cb7bff24110ba06bc312f6eaf3a1cd21bc947c92
shiro16 <nyanyanyawan24@gmail.com>
2015-12-30 00:46:40 +0900

    fixed serve method

--- lib/log-analyzer/router.rb
+++ lib/log-analyzer/router.rb
@@ -1,11 +1,7 @@
 module Log::Analyzer
-  class Mapper
-    def initialize(routes)
-      @routes = routes
-    end
-
+  class Router < ActionDispatch::Journey::Router
     def serve(req)
-      @routes.send(:find_routes, req)
+      find_routes(req)
     end
   end
 end
