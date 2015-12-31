9b87d024927ce1e5db78c87a419587c2c90c87a3
shiro16 <nyanyanyawan24@gmail.com>
2016-01-01 03:45:55 +0900

    define regexp const

--- lib/log-analyzer/log.rb
+++ lib/log-analyzer/log.rb
@@ -1,12 +1,14 @@
 module Log::Analyzer
   class Log
+    LOG_REGEXP = /Started (?<request_method>GET|POST|DELETE|PATCH|PUT) "(?<path_info>[^"]+)"/
+
     def initialize(path)
       @file = File.open(path)
     end
 
     def each_line(&block)
       @file.each_line do |line|
-        next unless md = line.match(/Started (?<request_method>GET|POST|DELETE|PATCH|PUT) "(?<path_info>[^"]+)"/)
+        next unless md = line.match(LOG_REGEXP)
         env = md.names.inject({}) do |hash, name|
                 hash[name.upcase] = md[name]
                 hash
--- lib/log-analyzer/routes.rb
+++ lib/log-analyzer/routes.rb
@@ -3,10 +3,11 @@ require 'action_dispatch'
 
 module Log::Analyzer
   class Routes < ActionDispatch::Journey::Routes
+    ROUTE_REGEXP = /(GET|POST|DELETE|PATCH|PUT)\s+([^\s]*)\s/
 
     def initialize(routes_text)
       super()
-      routes_text.scan(/(GET|POST|DELETE|PATCH|PUT)\s+([^\s]*)\s/) do |request_method, request_path|
+      routes_text.scan(ROUTE_REGEXP) do |request_method, request_path|
         path_pattern = Pattern.from_string(request_method, request_path)
         add_route(nil, path_pattern, {request_method: /^#{request_method}$/}, {}, {})
       end
