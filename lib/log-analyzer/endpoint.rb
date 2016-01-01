9a6c37a74fce701e1487521ed68c615955b69660
shiro16 <nyanyanyawan24@gmail.com>
2016-01-01 14:54:07 +0900

    add default value

--- lib/log-analyzer/endpoint.rb
+++ lib/log-analyzer/endpoint.rb
@@ -2,18 +2,18 @@ module Log::Analyzer
   class Endpoint
     attr_accessor :method, :uri_pattern, :count, :values
 
-    def initialize(method:, uri_pattern:, values: Hash.new(0))
+    def initialize(method: "", uri_pattern: "", values: Hash.new(0))
       @method      = method
       @uri_pattern = uri_pattern
       @count       = 0
       @values      = values
     end
 
-    def self.create(method:, uri_pattern:)
+    def self.create(method: "", uri_pattern: "")
       endpoints["#{method}:#{uri_pattern}"] = self.new(method: method, uri_pattern: uri_pattern)
     end
 
-    def self.find_by(method:, uri_pattern:)
+    def self.find_by(method: "", uri_pattern: "")
       endpoints["#{method}:#{uri_pattern}"]
     end
 
